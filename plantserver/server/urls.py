b54a09bbcc2585f4b5854d579e6d22c220b8ea75
Daniel Borner <71730916+DBorner@users.noreply.github.com>
2024-01-14 18:22:42 +0100

    Update urls.py

--- plantserver/server/urls.py
+++ plantserver/server/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("websocket/", views.websocket_view),
     path("plants/", views.PlantApiView.as_view()),
     path("probes/daily/", views.ProbeDailyApiView.as_view()),
     path("probes/current/", views.ProbeCurrentReadingsApiView.as_view()),
